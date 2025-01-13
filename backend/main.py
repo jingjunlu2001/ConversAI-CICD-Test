22ada6bcd2eb24c061b237d3f839b2d4f95eb872
jingjunlu2001 <65446882+jingjunlu2001@users.noreply.github.com>
2025-01-13 21:44:45 +0800

    Add files via upload

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from typing import Union
+from pydantic import BaseModel
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return "ConversAI Backend"
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
