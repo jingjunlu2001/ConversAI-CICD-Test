812d73f04355fc04f96fc54bc1c8a5701afc13ef
jingjunlu2001 <65446882+jingjunlu2001@users.noreply.github.com>
2025-01-14 21:52:06 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -16,5 +16,11 @@ async def read_root():
 async def health():
     return {"status": "ok"}
 
+
+@app.get("/healthy")
+async def healthy():
+    return "Healthy"
+    
+
 if __name__ == "__main__":
-    uvicorn.run(app, port=8000, host="0.0.0.0")
+    uvicorn.run(app, port=8080, host="0.0.0.0")
